f427950a6be5e4b09d67a0e02bb6b40f85f2e677
Briton Otieno <ojbrot@gmail.com>
2025-08-08 15:34:59 +0300

    Add the api base controller

---
+++ app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,43 @@
+class Api::V1::BaseController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :authenticate_api_user!
+
+  respond_to :json
+
+  protected
+
+  def authenticate_api_user!
+    token = request.headers['Authorization']&.split(' ')&.last
+    return render_unauthorized unless token
+
+    decoded_token = decode_jwt_token(token)
+    return render_unauthorized unless decoded_token
+
+    @current_user = User.find_by(id: decoded_token['user_id'])
+    return render_unauthorized unless @current_user&.active?
+  end
+
+  def current_user
+    @current_user
+  end
+
+  def render_unauthorized
+    render json: { error: 'Unauthorized' }, status: :unauthorized
+  end
+
+  def render_not_found(resource = 'Resource')
+    render json: { error: "#{resource} not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity(errors)
+    render json: { errors: errors }, status: :unprocessable_entity
+  end
+
+  private
+
+  def decode_jwt_token(token)
+    JWT.decode(token, Rails.application.secret_key_base, true, algorithm: 'HS256')[0]
+  rescue JWT::DecodeError
+    nil
+  end
+end
